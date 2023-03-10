077bbcd8aafb45f7d5769a17b27d40379829ef77
Have Samuel <samiehave9@gmail.com>
2023-03-11 00:47:48 +0300

    Resetting the inputs

--- index.js
+++ index.js
@@ -10,11 +10,31 @@ const updateMemeCanvas = (canvas, image, topText, bottomText) => {
   const ctx = canvas.getContext('2d');
   const width = image.width;
   const height = image.height;
+  const yOffset = height / 25;
+  const fontSize = Math.floor(height / 10);
 
   // Update canvas background
   canvas.width = width;
   canvas.height = height;
   ctx.drawImage(image, 0, 0);
+
+  // Update top text
+  ctx.font = `${fontSize}px Impact`;
+  ctx.textAlign = 'center';
+  ctx.fillStyle = 'white';
+  ctx.strokeStyle = 'black';
+  ctx.lineJoin = 'round';
+  ctx.lineWidth = Math.floor(fontSize / 4);
+
+  // Update top text
+  ctx.textBaseline = 'top';
+  ctx.strokeText(topText, width / 2, yOffset);
+  ctx.fillText(topText, width / 2, yOffset);
+
+  // Update bottom text
+  ctx.textBaseline = 'bottom';
+  ctx.strokeText(bottomText, width / 2, height - yOffset);
+  ctx.fillText(bottomText, width / 2, height - yOffset);
 };
 
 imageUrlInput.addEventListener('change', () => {
@@ -26,6 +46,14 @@ imageUrlInput.addEventListener('change', () => {
   image.addEventListener('load', () => {
     updateMemeCanvas(canvas, image, topTextInput.value, bottomTextInput.value);
   }, { once: true });
+});
+
+topTextInput.addEventListener('change', () => {
+  updateMemeCanvas(canvas, image, topTextInput.value, bottomTextInput.value);
+});
+
+bottomTextInput.addEventListener('change', () => {
+  updateMemeCanvas(canvas, image, topTextInput.value, bottomTextInput.value);
 });
 
 form.addEventListener('submit', (e) => {
